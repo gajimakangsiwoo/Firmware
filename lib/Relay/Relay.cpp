892971943eb444e3a14f2313ef72c562af319037
bitbyte08 <me@bitworkspace.kr>
2025-10-05 16:18:08 +0900

    feat :: getState 구현

--- lib/Relay/Relay.cpp
+++ lib/Relay/Relay.cpp
@@ -3,7 +3,7 @@
 //
 #include "Relay.h"
 
-Relay::Relay(int pin) : _pin(pin) {};
+Relay::Relay(int pin) : _pin(pin), _state(false) {};
 
 void Relay::begin() {
   pinMode(_pin, OUTPUT);
@@ -14,3 +14,5 @@ void Relay::setState(bool state) {
   _state = state;
   digitalWrite(_pin, _state);
 }
+
+bool Relay::getState() const { return _state; }
